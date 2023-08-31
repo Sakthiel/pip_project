f63b153681bd1c25d74199c2a01f49eebc709404
Sakthi Priyan Elangovan <sakthipriyan.e@thoughtworks.com>
2023-08-31 18:54:09 +0530

    Refactor. Added Error Response

--- src/main/java/com/thoughtworks/sample/version/view/VersionController.java
+++ src/main/java/com/thoughtworks/sample/version/view/VersionController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.thoughtworks.sample.version.VersionService;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
